cc32ceec723e33a7336da0b918389588fc5df3b5
Daisuke Shimamoto <diskshima@gmail.com>
2020-07-05 00:24:24 +0900

    Add audio-recorder-polyfill

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('hello world');
+import AudioRecorder from 'audio-recorder-polyfill';
+window.MediaRecorder = AudioRecorder;
+
+const recordButton = document.getElementById('record');
+const stopButton = document.getElementById('stop');
+
+var recorder;
+var recordedData;
+
+recordButton.addEventListener('click', () => {
+  // Request permissions to record audio
+  navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+    recorder = new MediaRecorder(stream)
+
+    // Set record to <audio> when recording will be finished
+    recorder.addEventListener('dataavailable', e => {
+      // audio.src = URL.createObjectURL(e.data)
+      recordedData = e.data;
+    })
+
+    // Start recording
+    recorder.start()
+  })
+});
+
+stopButton.addEventListener('click', () => {
+  // Stop recording
+  recorder.stop()
+  // Remove "recording" icon from browser tab
+  recorder.stream.getTracks().forEach(i => i.stop())
+});
